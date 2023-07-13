0515e21cfab2abaed9191f8056ff6f5c2ba4381a
JalilSa <95229047+JalilSa@users.noreply.github.com>
2023-07-13 18:19:25 -0300

    login con Passport

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ import MessageManagerDB from './dao/MessageManagerDB.js'
 import {UserModel} from './dao/models/Usermodel.js';
 import bcrypt from 'bcrypt';
 import { authMiddleware } from './middlewares/auth.js';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as GitHubStrategy } from 'passport-github2';
 
 const app = express();
 connectDB();
@@ -30,6 +33,70 @@ try {
 }
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// setup express session
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false, maxAge: 7200000 } // 2 horas
+}));
+
+//config de passport
+
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      const user = await UserModel.findOne({ username });
+      if (!user) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) return done(null, false);
+      return done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  }
+));
+
+passport.use(new GitHubStrategy({
+  clientID: "Iv1.6a7eeb5d8e282978",
+  clientSecret: "163ad5605b7342ce8e1836861d1d176d6af8432a",
+  callbackURL: "http://localhost:8080/auth/github/callback"
+},
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await UserModel.findOne({ githubId: profile.id });
+      if (!user) {
+        user = new UserModel({ 
+          githubId: profile.id, 
+          username: profile.username,
+        });
+        await user.save();
+      }
+      return done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  }
+));
+
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await UserModel.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 //setup handlebars
@@ -69,19 +136,19 @@ app.get('/home', (req, res) => {
 
 app.get('/chat', async (req, res) => {
   if (req.session.user) {
-    res.render('chat', { user: req.session.user });
     let messages = await messageManager.getMessages();
-    res.render('chat', { messages });
+    res.render('chat', { user: req.session.user, messages: messages });
   } else {
     // Si no hay usuario logueado, redirigir al login
     res.redirect('/login');
-}});
+  }
+});
+
 
 
 app.get('/realTimeProducts', (req, res) => {
   if (req.session.user) {
-    res.render('realTimeProducts', {products: products});
-    res.render('chat', { messages });
+    res.render('realTimeProducts', { user: req.session.user, products: products });
   } else {
     // Si no hay usuario logueado, redirigir al login
     res.redirect('/login');
@@ -91,6 +158,33 @@ app.get('/realTimeProducts', (req, res) => {
 app.get('/register' , (req,res)=> {
   res.render('register')
 })
+
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const user = new UserModel({ username, email, password: hashedPassword });
+
+  try {
+    await user.save();
+    res.redirect('/login');
+  } catch (err) {
+    res.status(400).send('Error al registrar');
+  }
+});
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+  req.session.user = req.user;
+  res.redirect('/');
+});
+
+
 
 app.get('/logout', authMiddleware, (req, res) => {
   req.session.destroy((err) => {
@@ -104,11 +198,6 @@ app.get('/logout', authMiddleware, (req, res) => {
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
-
-  // La contraseña de administrador debe ser un hash guardado de manera segura, 
-  // no una cadena de texto en el código.
- 
-
   const user = await UserModel.findOne({ username: username });
 
   if (user && bcrypt.compareSync(password, user.password)) {
@@ -121,6 +210,11 @@ app.post('/login', async (req, res) => {
 
 app.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send('El correo electrónico y la contraseña son obligatorios');
+  }
+
   const hashedPassword = bcrypt.hashSync(password, 10);
 
   try {
@@ -146,6 +240,7 @@ app.post('/register', async (req, res) => {
     }
   }
 });
+
 
 
 
@@ -180,7 +275,7 @@ io.on('connection', (socket) => {
 
   
 });
-//Crear usuario de prueba
+//Crear usuario de prueba, normalmente no agregaria el nombre de usuario y contraseña al ser un gran error de seguridad, pero no tengo otra forma de afectar la base de datos que se encuentra en la computadora donde se vaya a probar el codigo
 const adminEmail = 'adminCoder@coder.com';
 const adminPassword = 'adminCod3r123';
 const hashedPassword = bcrypt.hashSync(adminPassword, 10);
@@ -206,4 +301,6 @@ UserModel.findOne({ email: adminEmail }).then(user => {
 }).catch(err => {
   console.error('Error comprobando la existencia del usuario administrador', err);
 });
+
+
 
--- dao/models/Usermodel.js
+++ dao/models/Usermodel.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, default: 'user' } 
-  });
-  
+    username: { type: String, unique: true },
+    email: { type: String },
+    password: { type: String },
+    githubId: { type: String }, 
+    role: { type: String, default: 'user' }
+});
 
-export const UserModel = mongoose.model('users', userSchema)
+// Añadir el índice compuesto
+userSchema.index({ email: 1, githubId: 1 }, { unique: true, sparse: true });
+
+export const UserModel = mongoose.model('users', userSchema);
